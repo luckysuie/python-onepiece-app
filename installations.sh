959371ba6bc9a2fdcf69d4909db0f7d2104cba00
Lakshmi Narayana <luckyashu1856@gmail.com>
2025-11-04 22:01:36 +0530

    added installations

--- installations.sh
+++ installations.sh
@@ -1,9 +1,22 @@
-#!/bin/bash
-sudo apt update -y
-wget -O docker.sh https://get.docker.com/
-sudo sh docker.sh
-sudo usermod -aG docker jenkins
-sudo usermod -aG docker $USER
-sudo systemctl restart docker
-sudo systemctl restart jenkins
-newgrp docker
+sudo apt update
+sudo apt install openjdk-21-jdk -y
+java -version
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+
+sudo apt update
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+az version
+
+sudo apt update
+sudo snap install kubectl --classic
+kubectl version --client
